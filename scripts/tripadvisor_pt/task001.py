1c7c5ffe11a6ae518817d43206ec01708f16cb75
Agostinho Ramos <agostinhopina095@gmail.com>
2024-11-24 01:59:05 +0000

    Do task by topic id

--- scripts/tripadvisor_pt/task001.py
+++ scripts/tripadvisor_pt/task001.py
@@ -15,31 +15,32 @@ class UserTask:
                 data = json.loads(payload)
                 print(f"Received message on '{msg.topic}': {data}")
                 
-                MouseHandler.move_and_click(200, 780, click=True)
-                MouseHandler.move_and_click(385, 48, click=True)
-                pag.write(data["target_url"])
-                pag.press('enter')
-                time.sleep(1)
-                MouseHandler.move_and_click(471, 149)
-                pag.hotkey('ctrl', 'shift', 'i')
-                time.sleep(1)
-                pag.hotkey('ctrl', 'l')
-                time.sleep(1)                
-                javascript_code = """
-                $$zta.load()
-                """
-                self.wa.execute_javascript_code(javascript_code.splitlines())
-                time.sleep(1)
-                javascript_code = """
-                const cj = JSON.parse($$zta.conf);
-                const sd = $$zta.scrapeData(cj.selectors);
-                $$zta.sendPostRequest($$zta.url, sd)
-                """
-                self.wa.execute_javascript_code(javascript_code.splitlines())
-                time.sleep(1)
-                pag.hotkey('enter')
-                time.sleep(1)
-                pag.hotkey('alt', 'f4')
+                if msg.topic == "/automationscam/process/t/0001":     
+                    MouseHandler.move_and_click(200, 780, click=True)
+                    MouseHandler.move_and_click(385, 48, click=True)
+                    pag.write(data["target_url"])
+                    pag.press('enter')
+                    time.sleep(1)
+                    MouseHandler.move_and_click(471, 149)
+                    pag.hotkey('ctrl', 'shift', 'i')
+                    time.sleep(1)
+                    pag.hotkey('ctrl', 'l')
+                    time.sleep(1)                
+                    javascript_code = """
+                    $$zta.load()
+                    """
+                    self.wa.execute_javascript_code(javascript_code.splitlines())
+                    time.sleep(1)
+                    javascript_code = """
+                    const cj = JSON.parse($$zta.conf);
+                    const sd = $$zta.scrapeData(cj.selectors);
+                    $$zta.sendPostRequest($$zta.url, sd)
+                    """
+                    self.wa.execute_javascript_code(javascript_code.splitlines())
+                    time.sleep(1)
+                    pag.hotkey('enter')
+                    time.sleep(1)
+                    pag.hotkey('alt', 'f4')
                 
             except json.JSONDecodeError:
                 print(f"Received non-JSON message on '{msg.topic}': {msg.payload}")
